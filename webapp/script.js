52d767a7e4c8b4cca1d2b268dbdf3f07d583bd1d
CAL-ZAJ <zajilbot@gmail.com>
2024-10-31 19:30:20 +0300

    Update script.js

--- webapp/script.js
+++ webapp/script.js
@@ -1,1 +1,16 @@
+// webapp/script.js
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.Telegram.WebApp) {
+        const telegramWebApp = window.Telegram.WebApp;
+        telegramWebApp.ready();
+        const userInfoDiv = document.getElementById("user-info");
+        const user = telegramWebApp.initDataUnsafe.user;
+
+        if (user) {
+            userInfoDiv.innerHTML = `<p><strong>Welcome, ${user.first_name}</strong></p>`;
+        } else {
+            userInfoDiv.innerHTML = "<p>Could not retrieve user information.</p>";
+        }
+    }
+});
 
